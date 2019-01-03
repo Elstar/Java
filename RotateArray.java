e99898580cc753aabca13a87e45f76a4ae8cdc4a
Elstar <sergiy.dmb@gmail.com>
2019-01-03 23:02:27 +0200

    Update RotateArray.java

--- RotateArray.java
+++ RotateArray.java
@@ -8,33 +8,43 @@ public class RotateArray {
     public static void main(String[] args) {
         int[][] array = {
                 {1, 2, 3, 4, 5, 6},
-                {1, 2, 3, 4, 5, 6},
-                {1, 2, 3, 4, 5, 6},
-                {1, 2, 3, 4, 5, 6},
-                {1, 2, 3, 4, 5, 6},
-                {1, 2, 3, 4, 5, 6}
+                {2, 2, 3, 4, 5, 6},
+                {3, 7, 3, 4, 5, 6},
+                {4, 2, 3, 9, 5, 6},
+                {5, 2, 3, 4, 5, 6},
+                {6, 2, 3, 4, 5, 6}
         };
+
+        int x = array.length;
+        int y = array[0].length-1;
 
 
         System.out.println(Arrays.deepToString(array));
 
-        RotateArrayIn90Degree(array);
+        RotateArrayIn90Degree(array, x, y);
         System.out.println("Массив повернутый на 90 градусов\n" + Arrays.deepToString(array));
 
-        RotateArrayIn90Degree(array);
+        RotateArrayIn90Degree(array, x, y);
         System.out.println("Массив повернутый на 180 градусов\n" + Arrays.deepToString(array));
 
-        RotateArrayIn90Degree(array);
+        RotateArrayIn90Degree(array, x, y);
         System.out.println("Массив повернутый на 270 градусов\n" + Arrays.deepToString(array));
     }
 
-    static void RotateArrayIn90Degree(int[][] array) {
-        int buffValue;
-        for (int i=0; i<array.length; i++) {
-            for (int j=i; j<array[i].length; j++) {
-                buffValue = array[i][j];
-                array[i][j] = array[j][i];
-                array[j][i] = buffValue;
+    static void RotateArrayIn90Degree(int[][] array, int x, int y) {
+        int buff1, buff2;
+        for (int i=0; i<x/2; i++) {
+            for (int j=i; j<y-i; j++) {
+                buff1 = array[j][y-i];
+                array[j][y-i] = array[i][j];
+
+                buff2 = array[y-i][y-j];
+                array[y-i][y-j] = buff1;
+
+                buff1 = array[y-j][i];
+                array[y-j][i] = buff2;
+
+                array[i][j] = buff1;
             }
         }
     }
